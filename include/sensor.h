c9579e2f36eac381f405d2be1ca9682c17e311bb
henrykviana <henrykviana@gmail.com>
2026-05-04 19:12:39 +0200

    refactored program to utilize headers of different files and use them in main.cpp (untested) & no need to have src/esp32 directory (to be deleted in future commits if deemed unnecessary)

---
+++ include/sensor.h
@@ -0,0 +1,7 @@
+#ifndef IMU_MANAGER_H
+#define IMU_MANAGER_H
+
+void initIMU();
+void getData(char* buffer, int size);
+
+#endif
--- src/sensor.cpp
+++ src/sensor.cpp
@@ -1,18 +1,14 @@
 #include <M5Unified.h>
 #include <Wire.h>
 #include <I2C_MPU6886.h>
+#include "sensor.h"
 
 I2C_MPU6886 IMU;
 const int SDA_pin = 26;
 const int SCL_pin = 32;
 
-const int polling_rate = 100;
 
-void setup() {
-    auto cfg = M5.config();
-    M5.begin(cfg);
-    Serial.begin(115200);
-
+void initIMU() {
     Wire.begin(SDA_pin, SCL_pin); // SDA, SCL pins for the IMU to M5 Atom Lite Grove Port
     IMU.begin();
 
@@ -20,9 +16,7 @@ void setup() {
     Serial.println("acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z\n");
 }
 
-void loop() {
-    M5.update(); // Keeps the internal state updated
-
+void getData(char* buffer, int size) {
     // Create variables to hold our data
     float ax, ay, az; // Accelerometer (Gravity/Tilt)
     float gx, gy, gz; // Gyroscope (Rotation speed)
@@ -31,8 +25,7 @@ void loop() {
     IMU.getAccel(&ax, &ay, &az);
     IMU.getGyro(&gx, &gy, &gz);
 
-    // Print the Accelerometer data to the Serial Monitor
-    Serial.printf("%.2f,%.2f,%.2f,%.2f,%.2f,%.2f\n", 
-                    ax, ay, az, gx, gy, gz);
-    delay(polling_rate); // Slows down text for readability
+    // Print the IMU data to Serial Monitor
+    snprintf(buffer, size, "%.2f,%.2f,%.2f,%.2f,%.2f,%.2f", 
+                            ax, ay, az, gx, gy, gz);
 } 
